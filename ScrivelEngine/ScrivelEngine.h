f309c5cbf95c4b12e145e87c671978ffcc97a05c
Yusuke Sakurai <kerokerokerop@gmail.com>
2014-02-09 21:49:32 +0900

    Initial Commit

---
+++ ScrivelEngine/ScrivelEngine.h
@@ -0,0 +1,13 @@
+//
+//  ScrivelEngine.h
+//  ScrivelEngine
+//
+//  Created by 桜井雄介 on 2014/02/09.
+//  Copyright (c) 2014年 scrivel. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ScrivelEngine : NSObject
+
+@end
---
+++ ScrivelEngine/ScrivelEngine.m
@@ -0,0 +1,13 @@
+//
+//  ScrivelEngine.m
+//  ScrivelEngine
+//
+//  Created by 桜井雄介 on 2014/02/09.
+//  Copyright (c) 2014年 scrivel. All rights reserved.
+//
+
+#import "ScrivelEngine.h"
+
+@implementation ScrivelEngine
+
+@end
